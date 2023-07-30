920e616dadd2dc95cd39e2c1af11058e01fb5de4
Álvaro Navarro <31630723+thesnakebite@users.noreply.github.com>
2023-07-30 19:03:55 +0200

    Class dark mode App

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Color custom
+        slate: { 990: "#0b101d" },
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
